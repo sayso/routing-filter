686daadc4c770c3c50192dce5ef4b9eff07e2d8d
bartes <bartosz.knapik@llp.pl>
2010-12-30 22:32:11 +0100

    fix for activesupport fork

--- lib/routing_filter.rb
+++ lib/routing_filter.rb
@@ -1,5 +1,5 @@
 require 'action_pack'
-require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/string/inflector'
 
 module RoutingFilter
   autoload :Filter,     'routing_filter/filter'
